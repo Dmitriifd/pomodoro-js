782c9253a8984f07278a571f12fb2d0d01cea6f6
Dmitriifd <idimka9@gmail.com>
2023-01-31 15:42:09 +0500

    timer

---
+++ js/timer.js
@@ -0,0 +1,26 @@
+import { alarm } from './alarm.js'
+import { state } from './state.js'
+
+const minutesElem = document.querySelector('.time__minutes')
+const secondsElem = document.querySelector('.time__seconds')
+
+const showTime = (seconds) => {
+	const min = Math.floor(seconds / 60)
+	const sec = seconds % 60
+  minutesElem.textContent = min < 10 ? `0${min}` : min
+  secondsElem.textContent = sec < 10 ? `0${sec}` : sec
+}
+
+export const startTimer = () => {
+  state.timeLeft--
+
+  showTime(state.timeLeft)
+
+  if (state.timeLeft > 0 && state.isActive) {
+    state.timerId = setTimeout(startTimer, 100)
+  }
+
+  if (state.timeLeft <= 0) {
+    alarm()
+  }
+}
